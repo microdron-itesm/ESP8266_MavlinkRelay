82eaf7c1c12bfe41a5f8d94f209bd20c3b780aae
Abiel <empoleom@gmail.com>
2021-03-22 22:06:03 -0600

    UDP Testing

--- src/main.cpp
+++ src/main.cpp
@@ -14,7 +14,7 @@ const unsigned int tcpPort = 14552;
 const unsigned int listenPort = 14551;
 const unsigned int sendPort = 14550;
 
-WiFiServer tcpServer(tcpPort);
+WiFiClient client;
 
 size_t msgSize = 0;
 
@@ -25,59 +25,66 @@ mavlink_message_t serialMsg;
 mavlink_status_t serialStatusUDP;
 
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(2000000, SERIAL_8N1);
-  //Serial.println("Starting connection");
+    // put your setup code here, to run once:
+    Serial.begin(230400, SERIAL_8N1);
+    //Serial.println("Starting connection");
 
-  WiFi.begin(ssid, password);
-  while(WiFi.status() != WL_CONNECTED){
-    delay(500);
-    //Serial.print(".");
-  }
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        //Serial.print(".");
+    }
 
-  //Serial.println("Connected");
+    //Serial.println("Connected");
 
-  udp.begin(listenPort);
-  sendUdp.begin(sendPort);
+    udp.begin(listenPort);
+    sendUdp.begin(sendPort);
 
-  tcpServer.begin();
+    client.setNoDelay(true);
+    client.setTimeout(10);
 }
 
 void loop() {
     auto packetSize = udp.parsePacket();
-    if(packetSize){
-      int len = udp.read(incomingPacket, sizeof(incomingPacket));
-      for(int i = 0; i < len; ++i){
-        if(mavlink_parse_char(MAVLINK_COMM_0, incomingPacket[i], &currentUdpMsg, &udpStatus)) {
-            msgSize = mavlink_msg_to_send_buffer(serialBuffer, &currentUdpMsg);
-            Serial.write(serialBuffer, msgSize);
+    while (packetSize) {
+        int len = udp.read(incomingPacket, sizeof(incomingPacket));
+        for (int i = 0; i < len; ++i) {
+            if (mavlink_parse_char(MAVLINK_COMM_0, incomingPacket[i], &currentUdpMsg, &udpStatus)) {
+                msgSize = mavlink_msg_to_send_buffer(serialBuffer, &currentUdpMsg);
+                Serial.write(serialBuffer, msgSize);
+                //Serial.flush();
+            }
         }
-      }
+        packetSize = udp.parsePacket();
     }
 
-    WiFiClient client = tcpServer.available();
-    if(client && client.connected()){
-        while(client.available()){
-            auto c = client.read();
-            if(mavlink_parse_char(MAVLINK_COMM_0, c, &currentTcpMsg, &tcpStatus)) {
-                msgSize = mavlink_msg_to_send_buffer(serialBuffer, &currentTcpMsg);
-                Serial.write(serialBuffer, msgSize);
+    while(client.available()){
+        char c = client.read();
+        if(mavlink_parse_char(MAVLINK_COMM_1, c, &currentTcpMsg, &tcpStatus)){
+            msgSize = mavlink_msg_to_send_buffer(serialBuffer, &currentTcpMsg);
+            Serial.write(serialBuffer, msgSize);
+            Serial.flush();
+        }
+    }
+
+//    if(udp.remoteIP() and !client.connected()){
+//        client.connect(udp.remoteIP(), tcpPort);
+//        client.setNoDelay(true);
+//    }
+
+    //Recv from serial
+    while (Serial.available()) {
+        uint8_t byte = Serial.read();
+        if (mavlink_parse_char(MAVLINK_COMM_2, byte, &serialMsg, &serialStatusUDP)) {
+            uint16_t msgLen = mavlink_msg_to_send_buffer(serialBuffer, &serialMsg);
+            if (serialMsg.sysid == 2 and client.availableForWrite()) {
+                client.write(serialBuffer, msgLen);
+                client.flush(5);
+            } else {
+                sendUdp.beginPacket(udp.remoteIP(), sendPort);
+                sendUdp.write(serialBuffer, msgLen);
+                sendUdp.endPacket();
             }
         }
     }
-
-  // // //Recv from serial
-  while(Serial.available()){
-    uint8_t byte = Serial.read();
-    if (mavlink_parse_char(MAVLINK_COMM_0, byte, &serialMsg, &serialStatusUDP)){
-        uint16_t msgLen = mavlink_msg_to_send_buffer(serialBuffer, &serialMsg);
-        if(serialMsg.sysid == 2){
-            tcpServer.write(serialBuffer, msgLen);
-        } else {
-            sendUdp.beginPacket(udp.remoteIP(), sendPort);
-            sendUdp.write(serialBuffer, msgLen);
-            sendUdp.endPacket();
-        }
-    }
-  }
 }
